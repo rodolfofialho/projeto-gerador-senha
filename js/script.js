6db382d814220dccc8c156cd7fc8a20de9297c6d
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-03-28 18:39:01 -0300

    atualizando página inicial e finalizando a lógica

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let sliderElement = document.querySelector("#slider");
+let button = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+
+
+let charset = "abcdefghijklmnopqrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?/@#$%&*(){}";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword () {
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i ){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+
+function copyPassword(){
+    alert("SENHA COPIADA COM SUCESSO!");
+    navigator.clipboard.writeText(novaSenha);
+}
+
